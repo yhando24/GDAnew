37cafdbfb4af2cb8319bf50f8ddaf1f72a509100
yoann <you@example.com>
2019-01-10 14:26:53 +0100

    create  returnPeriodBetweenTwoDates in Global

--- src/main/java/com/GDA/utils/Global.java
+++ src/main/java/com/GDA/utils/Global.java
@@ -1,9 +1,23 @@
 package main.java.com.GDA.utils;
 
+
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.temporal.ChronoUnit;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Global {
 
-	public static int  returnPeriodBetweenTwoDate() {
-		return 0;
+	public static int  returnPeriodBetweenTwoDates(Date dateStart, Date dateEnd) {
+		
+		
+		
+		Duration period = Duration.between(LocalDate.of(dateStart.getYear(), dateStart.getMonth(), dateStart.getDay()),LocalDate.of(dateEnd.getYear(), dateEnd.getMonth(), dateEnd.getDay());
+		System.out.println(period.toDays());
+		return (int) period.toDays();
 		
 	}
 }
