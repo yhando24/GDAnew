25547d09b8d7a2ef9df39b88fd8d7adebd2f627c
yoann <you@example.com>
2019-01-14 13:58:41 +0100

    update  returnPeriodBetweenTwoDates

--- src/main/java/com/GDA/utils/Global.java
+++ src/main/java/com/GDA/utils/Global.java
@@ -1,5 +1,6 @@
 package main.java.com.GDA.utils;
 
+import java.time.DayOfWeek;
 import java.time.LocalDate;
 import java.time.Period;
 import java.util.Date;
@@ -12,12 +13,21 @@ public class Global {
 		LocalDate start = LocalDate.parse(date1);
 		String date2 = dateEnd.toString().substring(0, 10);
 		LocalDate end = LocalDate.parse(date2);
-		System.out.println(end +   "       " + start);
+		
+		
+		LocalDate varDate = dateStart;
+		
 		Period period = Period.between(LocalDate.of(start.getYear(), start.getMonth(), start.getDayOfMonth()),LocalDate.of(end.getYear(), end.getMonth(), end.getDayOfMonth()));
 		System.out.println(period.getDays());
-		
-		return (int) period.getDays() + 1;
-		
+		int nbrOfWorkday = 1;
+		for(int i = 0; i<(int) period.getDays(); i++) {
+			varDate = varDate.plusDays(1);
+			if (!(varDate.getDayOfWeek() == DayOfWeek.SATURDAY ||
+					varDate.getDayOfWeek() == DayOfWeek.SUNDAY)) {
+		            ++nbrOfWorkday;
+		        }
+		}	
+		return nbrOfWorkday;
 	}
 	
 }
