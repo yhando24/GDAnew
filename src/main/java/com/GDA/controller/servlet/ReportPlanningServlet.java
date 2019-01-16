742c47e21b170fc43565f4da5e1f2b14c3fe96e9
Delteil <a.delteil83@gmail.com>
2019-01-16 13:44:07 +0100

    update servlet et DAO pr reportPlanning

--- src/main/java/com/GDA/controller/servlet/ReportPlanningServlet.java
+++ src/main/java/com/GDA/controller/servlet/ReportPlanningServlet.java
@@ -1,6 +1,8 @@
 package main.java.com.GDA.controller.servlet;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -11,6 +13,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 import main.java.com.GDA.bean.User;
+import main.java.com.GDA.model.dao.AbsenceForReport.AbsenceForReportDAO;
 
 /**
  * Servlet implementation class ReportServlet
@@ -18,42 +21,60 @@ import main.java.com.GDA.bean.User;
 @WebServlet("/report-planning")
 public class ReportPlanningServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ReportPlanningServlet() {
-        super();
-        
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		HttpSession session = request.getSession();
+	public ReportPlanningServlet() {
+		super();
 
-		User user = (User) session.getAttribute("user");
-		
-		if(user.getFunction().getName().equals("manager")) {
-			System.out.println("plop");
-			RequestDispatcher dispatcher = getServletContext()
-					.getRequestDispatcher("/WEB-INF/view/reportPlanning.jsp");
-			dispatcher.forward(request, response);
-		}else {
-			int plop;
-			response.sendRedirect(request.getContextPath() + "/indexEmployee"); 	
-		}
-		
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+
+		User user = (User) session.getAttribute("user");
+
+		if (user.getFunction().getName().equals("manager")) {
+			System.out.println("plop");
+			RequestDispatcher dispatcher = getServletContext().getRequestDispatcher("/WEB-INF/view/reportPlanning.jsp");
+			dispatcher.forward(request, response);
+		} else {
+			int plop;
+			response.sendRedirect(request.getContextPath() + "/indexEmployee");
+		}
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession session = request.getSession();
+		String year = request.getParameter("year");
+		String month = request.getParameter("month");
+		String dep = request.getParameter("departement");
+		Map<String, String> selectReportPlanning = new HashMap<String, String>();
+		selectReportPlanning.put("year", year);
+		selectReportPlanning.put("month", month);
+		selectReportPlanning.put("departement", dep);
+		session.setAttribute("selectReport", selectReportPlanning);
+
+		AbsenceForReportDAO abs = new AbsenceForReportDAO();
+
+		abs.findAllAbsencesByNameDepartementMonthAndYear(Integer.parseInt(dep), month, year);
+
+		response.sendRedirect(request.getContextPath() + "/report-planning");
+
 	}
 
 }
--- src/main/java/com/GDA/model/dao/AbsenceForReport/AbsenceForReportDAO.java
+++ src/main/java/com/GDA/model/dao/AbsenceForReport/AbsenceForReportDAO.java
@@ -12,56 +12,105 @@ import main.java.com.GDA.bean.AbsenceForReport;
 import main.java.com.GDA.utils.ConnectionDB;
 
 public class AbsenceForReportDAO {
-	public List<AbsenceForReport> findAllAbsencesByDepartementMonthAndYear(int idDep , String month,String year ){
 
-		   List<AbsenceForReport> absences = new ArrayList<AbsenceForReport>();
+	public List<AbsenceForReport> findAllAbsencesByDepartementMonthAndYear(int idDep, String month, String year) {
 
-		   Connection connection = null;
-		   PreparedStatement prepareStatement = null;
+		List<AbsenceForReport> absences = new ArrayList<AbsenceForReport>();
 
-		       try {
-		           connection = ConnectionDB.getConnection();
-		          // String query = "SELECT absence.id as idAbsence, startDate , endDate , reason , idAbsenceType , idStatus , idUser , user.name ,user.firstname ,status.name as namestatus , absencetype.name as nameType FROM absence JOIN user ON user.id = absence.idUser JOIN status ON idStatus = status.id JOIN absencetype on absencetype.id = idAbsencetype WHERE user.idDepartement = ? AND absence.idStatus = 2 ORDER BY idUser";
-		           String query = "SELECT startDate , (DAYOFYEAR(endDate) - DAYOFYEAR(startDate) +1)  as duration , DAY( LAST_DAY(startDate)) as lastDay FROM absence JOIN user ON user.id = absence.idUser JOIN departement ON departement.id = user.idDepartement WHERE user.idDepartement = ? AND YEAR(startDate) = ? AND  MONTH(startDate) = ?"; 
-		      
-		           
-		           prepareStatement = connection.prepareStatement(query);
+		Connection connection = null;
+		PreparedStatement prepareStatement = null;
 
-		           prepareStatement.setInt(1,idDep );
-		           prepareStatement.setString(2,year );
-		           prepareStatement.setString(3,month );
-		           ResultSet resultSet = prepareStatement.executeQuery();
+		try {
+			connection = ConnectionDB.getConnection();
+			// String query = "SELECT absence.id as idAbsence, startDate , endDate , reason
+			// , idAbsenceType , idStatus , idUser , user.name ,user.firstname ,status.name
+			// as namestatus , absencetype.name as nameType FROM absence JOIN user ON
+			// user.id = absence.idUser JOIN status ON idStatus = status.id JOIN absencetype
+			// on absencetype.id = idAbsencetype WHERE user.idDepartement = ? AND
+			// absence.idStatus = 2 ORDER BY idUser";
+			String query = "SELECT startDate , (DAYOFYEAR(endDate) - DAYOFYEAR(startDate) +1)  as duration , DAY( LAST_DAY(startDate)) as lastDay FROM absence JOIN user ON user.id = absence.idUser JOIN departement ON departement.id = user.idDepartement WHERE user.idDepartement = ? AND YEAR(startDate) = ? AND  MONTH(startDate) = ?";
 
-		     
-		     
-		           while (resultSet.next()){
-		              
-		        	   AbsenceForReport absence = new AbsenceForReport();
-			               
-			            
-		                absence.setDateStart(LocalDate.parse(resultSet.getString("startDate")));
-		                absence.setDuration(resultSet.getInt("lastDay")); //lastDay
-		                absence.setLastDayOfMonth(resultSet.getInt("lastDay"));
-		       
-		                absences.add(absence);
-		       
-		             
-		           }
-		       }
-		       catch (Exception e){
-		           e.printStackTrace();
-		       }
-		       finally{
-		           try {
-		               prepareStatement.close();
-		               connection.close();
-		           } catch (SQLException e) {
-		               // ne rien faire
-		               e.printStackTrace();
-		           }
-		       }
-		       return absences;
+			prepareStatement = connection.prepareStatement(query);
 
+			prepareStatement.setInt(1, idDep);
+			prepareStatement.setString(2, year);
+			prepareStatement.setString(3, month);
+			ResultSet resultSet = prepareStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				AbsenceForReport absence = new AbsenceForReport();
+
+				absence.setDateStart(LocalDate.parse(resultSet.getString("startDate")));
+				absence.setDuration(resultSet.getInt("lastDay")); // lastDay
+				absence.setLastDayOfMonth(resultSet.getInt("lastDay"));
+
+				absences.add(absence);
+
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				prepareStatement.close();
+				connection.close();
+			} catch (SQLException e) {
+				// ne rien faire
+				e.printStackTrace();
+			}
 		}
+		return absences;
 
+	}
+
+	public List<AbsenceForReport> findAllAbsencesByNameDepartementMonthAndYear(int idDep, String month, String year) {
+
+		List<AbsenceForReport> absencesPlanning = new ArrayList<AbsenceForReport>();
+
+		Connection connection = null;
+		PreparedStatement prepareStatement = null;
+
+		try {
+			connection = ConnectionDB.getConnection();
+			// String query = "SELECT absence.id as idAbsence, startDate , endDate , reason
+			// , idAbsenceType , idStatus , idUser , user.name ,user.firstname ,status.name
+			// as namestatus , absencetype.name as nameType FROM absence JOIN user ON
+			// user.id = absence.idUser JOIN status ON idStatus = status.id JOIN absencetype
+			// on absencetype.id = idAbsencetype WHERE user.idDepartement = ? AND
+			// absence.idStatus = 2 ORDER BY idUser";
+//		           String query = "SELECT startDate , (DAYOFYEAR(endDate) - DAYOFYEAR(startDate) +1)  as duration , DAY( LAST_DAY(startDate)) as lastDay FROM absence JOIN user ON user.id = absence.idUser JOIN departement ON departement.id = user.idDepartement WHERE user.idDepartement = ? AND YEAR(startDate) = ? AND  MONTH(startDate) = ?"; 
+
+			String query = "SELECT user.name, startDate, endDate, DAY( LAST_DAY(startDate)) as lastDay FROM absence JOIN user ON user.id = absence.idUser JOIN departement ON departement.id = user.idDepartement WHERE user.idDepartement = ? AND YEAR(startDate) = ? AND MONTH(startDate) = ?";
+
+			prepareStatement = connection.prepareStatement(query);
+
+			prepareStatement.setInt(1, idDep);
+			prepareStatement.setString(2, year);
+			prepareStatement.setString(3, month);
+			ResultSet resultSet = prepareStatement.executeQuery();
+
+			while (resultSet.next()) {
+
+				AbsenceForReport absencePlanning = new AbsenceForReport();
+
+				absencePlanning.setDateStart(LocalDate.parse(resultSet.getString("startDate")));
+				absencePlanning.setDuration(resultSet.getInt("lastDay")); // lastDay
+				absencePlanning.setLastDayOfMonth(resultSet.getInt("lastDay"));
+
+				absencesPlanning.add(absencePlanning);
+			}
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		} finally {
+			try {
+				prepareStatement.close();
+				connection.close();
+			} catch (SQLException e) {
+				// ne rien faire
+				e.printStackTrace();
+			}
+		}
+		return absencesPlanning;
+	}
 }
