017c5e6958484eb4a9c87bbdad10eb20be85d9a3
formation <formation@DESKTOP-H3GKILC.DIGINAMIC.LOCAL>
2019-03-12 11:42:12 +0100

    Changement version hibernate validator

--- src/main/java/com/GDA/bean/User.java
+++ src/main/java/com/GDA/bean/User.java
@@ -13,6 +13,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 
 import org.hibernate.validator.constraints.NotBlank;
 
